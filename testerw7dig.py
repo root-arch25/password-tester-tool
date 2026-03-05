3f4aecb9325a6e23335cb9a97a6adfd6d35a892d
root-arch25 <root.arch_code@proton.me>
2026-03-05 15:21:13 +0100

    Update testerw7dig.py

--- testerw7dig.py
+++ testerw7dig.py
@@ -1,6 +1,6 @@
 import itertools 
 
-print("make a password with 10 letters/digits")
+print("make a password with 7 letters/digits")
 passwd = input("password: ")
 
 nums = ['0','1','2','3','4','5','6','7','8','9',
