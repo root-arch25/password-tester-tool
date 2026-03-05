c70b04f0fb2b3ed40a32bce033c174429d1f670f
root-arch25 <root.arch_code@proton.me>
2026-03-05 14:51:02 +0100

    Create testerw5dig.py

---
+++ testerw5dig.py
@@ -0,0 +1,16 @@
+import itertools 
+
+print("make a password with 10 letters/digits")
+passwd = input("password: ")
+
+nums = ['0','1','2','3','4','5','6','7','8','9',
+        'a','b','c','d','e','f','g','h','i','j',
+        'k','l','m','n','o','p','q','r','s','t',
+        'u','v','w','x','y','z']
+for p in itertools.product(nums, repeat=5):
+    number = (''.join(map(str, p)))
+    print(number)
+
+    if number == passwd:
+	    print("password found:", passwd)
+	    break
